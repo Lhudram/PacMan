652aa2606237ac9ec98d8f62d65265ff8b2c2474
Lhudram <martdulh@hotmail.fr>
2019-03-27 14:37:10 +0100

    init

---
+++ src/Modele/SimplePacMan.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modele;
+
+import java.util.Observable;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ *
+ * @author fred
+ */
+public class SimplePacMan extends Observable implements Runnable {
+
+    private int x, y, sizeX, sizeY;
+    
+    private Random r = new Random();
+    
+    
+    public SimplePacMan(int _sizeX, int _sizeY) {
+        x = 0; y = 0;
+        
+        sizeX = _sizeX;
+        sizeY = _sizeY;
+       
+    }
+    
+    public int getX() {
+        return x;
+    }
+    
+    public int getY() {
+        return y;
+    }
+    
+    public void start() {
+        new Thread(this).start();
+    }
+
+    public void setxy(int newx,int newy){
+        if(coupPossible(newx,newy)) {
+            x += newx;
+            y += newy;
+        }
+    }
+
+    public boolean coupPossible(int newx,int newy){
+        if (x + newx >= 0 && x + newx < sizeX && y + newy >= 0 && y + newy < sizeY)
+            return true;
+        return false;
+    }
+
+    public void initXY() {
+        x = 0;
+        y = 0;
+    }
+    
+    @Override
+    public void run() {
+        while(true) { // spm descent dans la grille à chaque pas de temps
+            
+           /*int deltaX = r.nextInt(2);
+
+
+
+           if (x + deltaX > 0 && x + deltaX < sizeX) {
+               x += deltaX;
+           }
+           
+           int deltaY = r.nextInt(2);
+           if (y + deltaY > 0 && y + deltaY < sizeY) {
+               y += deltaY;
+           } 
+
+           */
+           System.out.println(x + " - " + y);
+           
+           setChanged(); 
+           notifyObservers(); // notification de l'observer
+           
+            try {
+                Thread.sleep(300); // pause
+            } catch (InterruptedException ex) {
+                Logger.getLogger(SimplePacMan.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    
+    }
+    
+    
+}
