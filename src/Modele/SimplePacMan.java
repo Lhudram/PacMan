6fe2c06221bb100a1d90d1493cc90e163e5ec77b
Lhudram <martdulh@hotmail.fr>
2019-04-03 16:18:34 +0200

    test ia fantome

--- src/Modele/SimplePacMan.java
+++ src/Modele/SimplePacMan.java
@@ -7,12 +7,11 @@ package Modele;
 
 import javafx.scene.input.KeyCode;
 
-import javax.swing.*;
 import java.util.Observable;
+import java.util.Random;
 
 
 /**
- *
  * @author fred
  */
 public class SimplePacMan extends Observable implements Runnable {
@@ -21,58 +20,59 @@ public class SimplePacMan extends Observable implements Runnable {
     private volatile boolean exit = false;
     private int x, y, sizeX, sizeY;
     private int[][] grille;//0=sol,1=mur,2=fruits,3=gommes,4=porte fantome
-    private int score;
     private boolean estInvincible;
+    private boolean estEnKillStreak;
     private int[] fantomex;
     private int[] fantomey;
-    private int score_dot;
+    private int score;
     private int score_f;
+    private int timerInvincible;
 
     public SimplePacMan(int _sizeX, int _sizeY) {
-        x = 10; 
-        y = 15;
-        score_dot=0;
-        score_f=0;
-        sizeX = _sizeX;
-        sizeY = _sizeY;
+        this.x = 10;
+        this.y = 15;
+        this.score = 0;
+        this.score_f = 0;
+        this.sizeX = _sizeX;
+        this.sizeY = _sizeY;
+        this.timerInvincible = 0;
 
-        this.nbrfantome=4;
-        this.fantomex=new int []{10,10,9,11};
-        this.fantomey=new int []{7,9,9,9};
-
-        this.score=0;
-        this.estInvincible=false;
+        this.nbrfantome = 4;
+        this.fantomex = new int[]{10, 10, 9, 11};
+        this.fantomey = new int[]{7, 9, 9, 9};
+        this.estInvincible = false;
+        this.estEnKillStreak = false;
 
         this.grille = new int[][]{
-                {0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0},
-                {1,1,1,1,1,1,1,0,1,0,1,0,1,1,1,1,1,1,1,1,1},
-                {1,2,3,2,2,2,1,0,1,0,1,0,1,2,2,3,1,2,2,2,1},
-                {1,2,1,2,1,2,1,0,1,0,1,0,1,2,1,2,2,2,1,2,1},
-                {1,2,1,2,1,2,1,1,1,0,1,1,1,2,1,1,1,2,1,2,1},
-                {1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,2,1},
-                {1,2,1,2,1,1,1,1,1,0,1,1,1,2,1,2,1,1,1,2,1},
-                {1,2,1,2,2,2,1,0,0,0,0,0,0,2,1,2,2,2,1,2,1},
-                {1,2,1,2,1,2,1,0,1,1,1,0,1,2,1,2,1,2,1,2,1},
-                {1,2,2,2,1,2,0,0,1,0,1,0,1,2,2,2,1,2,2,2,1},
-                {1,1,1,2,1,1,1,0,4,0,1,0,1,1,1,2,1,1,1,2,1},
-                {1,2,2,2,1,2,0,0,1,0,1,0,1,2,2,2,1,2,2,2,1},
-                {1,2,1,2,1,2,1,0,1,1,1,0,1,2,1,2,1,2,1,2,1},
-                {1,2,1,2,2,2,1,0,0,0,0,0,0,2,1,2,2,2,1,2,1},
-                {1,2,1,2,1,1,1,1,1,0,1,1,1,2,1,2,1,1,1,2,1},
-                {1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,2,1},
-                {1,2,1,2,1,2,1,1,1,0,1,1,1,2,1,1,1,2,1,2,1},
-                {1,2,1,2,1,2,1,0,1,0,1,0,1,2,1,2,2,2,1,2,1},
-                {1,2,3,2,2,2,1,0,1,0,1,0,1,2,2,3,1,2,2,2,1},
-                {1,1,1,1,1,1,1,0,1,0,1,0,1,1,1,1,1,1,1,1,1},
-                {0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0}
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 2, 3, 2, 2, 2, 1, 0, 1, 0, 1, 0, 1, 2, 2, 3, 1, 2, 2, 2, 1},
+                {1, 2, 1, 2, 1, 2, 1, 0, 1, 0, 1, 0, 1, 2, 1, 2, 2, 2, 1, 2, 1},
+                {1, 2, 1, 2, 1, 2, 1, 1, 1, 0, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1},
+                {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1},
+                {1, 2, 1, 2, 1, 1, 1, 1, 1, 0, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1},
+                {1, 2, 1, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 2, 1, 2, 1},
+                {1, 2, 1, 2, 1, 2, 1, 0, 1, 1, 1, 0, 1, 2, 1, 2, 1, 2, 1, 2, 1},
+                {1, 2, 2, 2, 1, 2, 0, 0, 1, 0, 1, 0, 1, 2, 2, 2, 1, 2, 2, 2, 1},
+                {1, 1, 1, 2, 1, 1, 1, 0, 4, 0, 1, 0, 1, 1, 1, 2, 1, 1, 1, 2, 1},
+                {1, 2, 2, 2, 1, 2, 0, 0, 1, 0, 1, 0, 1, 2, 2, 2, 1, 2, 2, 2, 1},
+                {1, 2, 1, 2, 1, 2, 1, 0, 1, 1, 1, 0, 1, 2, 1, 2, 1, 2, 1, 2, 1},
+                {1, 2, 1, 2, 2, 2, 1, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 2, 1, 2, 1},
+                {1, 2, 1, 2, 1, 1, 1, 1, 1, 0, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1},
+                {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1},
+                {1, 2, 1, 2, 1, 2, 1, 1, 1, 0, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1},
+                {1, 2, 1, 2, 1, 2, 1, 0, 1, 0, 1, 0, 1, 2, 1, 2, 2, 2, 1, 2, 1},
+                {1, 2, 3, 2, 2, 2, 1, 0, 1, 0, 1, 0, 1, 2, 2, 3, 1, 2, 2, 2, 1},
+                {1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
         };
     }
+
     @Override
     public void run() {
-        while(!exit) {
+        while (!exit) {
             setChanged();
             notifyObservers(); // notification de l'observer
-
             try {
                 Thread.sleep(300); // pause
             } catch (InterruptedException ex) {
@@ -85,128 +85,181 @@ public class SimplePacMan extends Observable implements Runnable {
     public void start() {
         new Thread(this).start();
     }
-    public void stop(){
+
+    public void stop() {
         this.exit = true;
-    }
-
-    public void deplacement(KeyCode direction){
-        int addx=0,addy=0;
-
-        switch(direction){
-            case UP:addx=0;addy=-1;
-                break;
-            case DOWN:addx=0;addy=1;
-                break;
-            case LEFT:addx=-1;addy=0;
-                break;
-            case RIGHT:addx=1;addy=0;
-                break;
-        }
-        if(x+addx>=0 && x+addx<sizeX && y+addy >=0 && y+addy<sizeY) {
-            if (!estMur(x+addx, y+addy)) {
-                x+=addx;
-                y+=addy;
-            }
-        }
-        else {
-            if (x + addx < 0) {
-                if (!estMur(20, y))
-                    x =20;
-            }
-            if (x + addx >= sizeX) {
-                if (!estMur(0, y))
-                    x =0;
-            }
-            if (y + addy >= sizeY) {
-                if (!estMur(x, 0))
-                    y=0;
-            }
-            if (y + addy < 0) {
-                if (!estMur(x, 20))
-                    y=20;
-            }
-        }
-    }
-
-    public void deplacementfantome() {
-    }
-
-    private boolean estMur(int newx, int newy){
-        if(newx!=x) {
-            if (grille[newx][y]==1 || grille[newx][y]==4 ) {
-               return true;
-            }
-        }
-        if(newy!=y) {
-            return grille[x][newy] == 1 || grille[x][newy] == 4;
-        }
-        return false;
-    }
-
-    public void initXY() {
-        x = 0;
-        y = 0;
-    }
-
-    public void collisionFantome() {
-        for(int i=0;i<nbrfantome;i++){
-            if(fantomex[i]==x && fantomey[i]==y){
-                if(estInvincible()){
-                    augmenterScore(2);
-                }
-                else{
-                    gameOver();
-                }
-            }
-        }
-
     }
 
     private void gameOver() {
         this.exit = true;
     }
 
-    private boolean estInvincible() {
+    public void deplacement(KeyCode direction) {
+        int addx = 0, addy = 0;
+
+        switch (direction) {
+            case UP:
+                addx = 0;
+                addy = -1;
+                break;
+            case DOWN:
+                addx = 0;
+                addy = 1;
+                break;
+            case LEFT:
+                addx = -1;
+                addy = 0;
+                break;
+            case RIGHT:
+                addx = 1;
+                addy = 0;
+                break;
+        }
+        if (x + addx >= 0 && x + addx < sizeX && y + addy >= 0 && y + addy < sizeY) {
+            if (!estMur(x + addx, y + addy)) {
+                x += addx;
+                y += addy;
+            }
+        } else {
+            if (x + addx < 0) {
+                if (!deplacementPossible(20, y))
+                    x = 20;
+            }
+            if (x + addx >= sizeX) {
+                if (!deplacementPossible(0, y))
+                    x = 0;
+            }
+            if (y + addy >= sizeY) {
+                if (!deplacementPossible(x, 0))
+                    y = 0;
+            }
+            if (y + addy < 0) {
+                if (!deplacementPossible(x, 20))
+                    y = 20;
+            }
+        }
+    }
+
+    public void deplacementfantome(int i) {
+        //haut,bas,droite,gauche
+        boolean[] listeDeplacement;
+        listeDeplacement = new boolean[]{false, false, false, false};
+        listeDeplacement[0] = !estMur(fantomex[i], fantomey[i] - 1);
+        listeDeplacement[1] = !estMur(fantomex[i], fantomey[i] + 1);
+        listeDeplacement[2] = !estMur(fantomex[i] + 1, fantomey[i]);
+        listeDeplacement[3] = !estMur(fantomex[i] - 1, fantomey[i]);
+
+        boolean estTrouve = false;
+        while (!estTrouve) {
+            int k = (int) (Math.random() * listeDeplacement.length);
+            if (listeDeplacement[k]) {
+                estTrouve = true;
+                switch (k) {
+                    case 0:
+                        fantomey[i] = fantomey[i] - 1;
+                        break;
+                    case 1:
+                        fantomey[i] = fantomey[i] + 1;
+                        break;
+                    case 2:
+                        fantomex[i] = fantomex[i] + 1;
+                        break;
+                    case 3:
+                        fantomex[i] = fantomex[i] - 1;
+                        break;
+                }
+            }
+        }
+    }
+
+    private boolean estMur(int x, int y) {
+        return grille[x][y] == 1;
+    }
+
+    private boolean deplacementPossible(int newx, int newy) {
+        if (newx != x) {
+            if (estMur(newx, y) || grille[newx][y] == 4) {
+                return true;
+            }
+        }
+        if (newy != y) {
+            return estMur(x, newy) || grille[x][newy] == 4;
+        }
+        return false;
+    }
+
+    public void collisionFantome() {
+        for (int i = 0; i < nbrfantome; i++) {
+            if (fantomex[i] == x && fantomey[i] == y) {
+                if (estInvincible()) {
+                    if (this.estEnKillStreak)
+                        augmenterScore(3);
+                    else {
+                        this.estEnKillStreak = true;
+                        augmenterScore(2);
+                    }
+                    fantomex[i] = 10;
+                    fantomey[i] = 9;
+                } else {
+                    gameOver();
+                }
+            }
+        }
+
+    }
+    public boolean caseAvecFantome() {
+        for (int i = 0; i < nbrfantome; i++) {
+            if (fantomex[i] == x && fantomey[i] == y) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void setInvincible() {
+        this.estInvincible = true;
+        this.estEnKillStreak = false;
+        this.timerInvincible = 20;
+        this.score_f = 0;
+    }
+
+    public boolean estInvincible() {
         return this.estInvincible;
     }
 
-    public void setTab(int i, int j ,int elem) {
+    public void decrementerTimer() {
+        this.timerInvincible--;
+        if (this.timerInvincible == 0) {
+            this.estInvincible = false;
+            this.score += this.score_f;
+        }
+    }
+
+    public void augmenterScore(int point) {
+        //point pac-dot
+        if (point == 0)
+            this.score += 10;
+            //power pellet
+        else if (point == 1)
+            this.score += 50;
+            //1er fantôme
+        else if (point == 2)
+            this.score += 200;
+        else
+            this.score_f += this.score_f * 2;
+    }
+
+
+    public void setTab(int i, int j, int elem) {
         this.grille[i][j] = elem;
     }
-    
+
     public int getTab(int i, int j) {
         return this.grille[i][j];
     }
-    
-    public void augmenterScore(int point){
-        //point pac-dot
-        if (point==0)
-            this.score_dot+=10;
-        //power pellet
-        else if(point==1)
-            this.score_dot+=50;
-        //1er fantôme
-        else if(point==2)
-            this.score_f+=200;
-        else
-            this.score_f=this.score_f*2;
-    }
 
-    public int getScoreDot() {
-        return this.score_dot;
-    }
-    
-    public int getScoreF() {
-        return this.score_f;
-    }
-    
-    public void setInvincible(){
-        this.estInvincible = true;
-        this.timerInvincible=
-    }
-
-    public void setNonInvicible(){
-        this.estInvincible=false;
+    public int getScore() {
+        return this.score;
     }
 
     public int getX() {
--- src/VueControleur/SimpleVC.java
+++ src/VueControleur/SimpleVC.java
@@ -1,40 +1,41 @@
-/* 
- * To change this license header, choose License Headers in Project Properties. 
- * To change this template file, choose Tools | Templates 
- * and open the template in the editor. 
- */ 
-package VueControleur; 
- 
-import Modele.SimplePacMan; 
-import java.util.Observer; 
-import javafx.scene.image.Image; 
-import javafx.application.Application; 
-import javafx.scene.Scene; 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VueControleur;
+
+import Modele.SimplePacMan;
+
+import java.util.Observer;
+
+import javafx.scene.image.Image;
+import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
-/** 
- * 
- * @author frederic.armetta 
- */ 
-public class SimpleVC extends Application { 
-     
-    private final int SIZE_X =21; 
+/**
+ * @author frederic.armetta
+ */
+public class SimpleVC extends Application {
+
+    private final int SIZE_X = 21;
     private final int SIZE_Y = 21;
     private int oldx = 0;
-    private int oldy =0;
-    private KeyCode direction=KeyCode.N;
+    private int oldy = 0;
+    private KeyCode direction = KeyCode.N;
 
 
-    @Override 
+    @Override
     public void start(Stage primaryStage) {
         SimplePacMan spm = new SimplePacMan(SIZE_X, SIZE_Y); // initialisation du modèle
-         
+
         GridPane root = new GridPane(); // création de la grille
-         
+
         // Pacman.svg.png 
         Image imPM = new Image("Pacman.png"); // préparation des images
         Image imSol = new Image("sol.png");
@@ -45,6 +46,7 @@ public class SimpleVC extends Application {
         Image imGhost1 = new Image("fantome_rouge.png");
         Image imGhost2 = new Image("fantome_orange.png");
         Image imGhost3 = new Image("fantome_vert.png");
+        Image imGhostAfraid = new Image("fantome_vulnerable.png");
 
         //img.setScaleY(0.01); 
         //img.setScaleX(0.01); 
@@ -52,13 +54,13 @@ public class SimpleVC extends Application {
         ImageView[][] tab = new ImageView[SIZE_X][SIZE_Y]; // tableau permettant de récupérer les cases graphiques lors du rafraichissement
 
         for (int i = 0; i < SIZE_X; i++) { // initialisation de la grille (sans image) 
-            for (int j = 0; j < SIZE_Y; j++) { 
-                ImageView img = new ImageView(); 
-                tab[i][j] = img; 
-                root.add(img, i, j); 
-                
-                switch (spm.getTab(i,j)){
-                    case 0 :
+            for (int j = 0; j < SIZE_Y; j++) {
+                ImageView img = new ImageView();
+                tab[i][j] = img;
+                root.add(img, i, j);
+
+                switch (spm.getTab(i, j)) {
+                    case 0:
                         tab[i][j].setImage(imSol);
                         break;
                     case 1:
@@ -71,83 +73,112 @@ public class SimpleVC extends Application {
                         tab[i][j].setImage(imGomme);
                         break;
                 }
-                    
-            } 
- 
-        } 
+
+            }
+
+        }
 
         // l'observer observe l'obervable (update est exécuté dès notifyObservers() est appelé côté modèle )
         Observer o = (o1, arg) -> {
             int[] fantomex = spm.getFantomex();
-            int[] fantomey= spm.getFantomey();
+            int[] fantomey = spm.getFantomey();
+            int[] oldfantomex;
+            int[] oldfantomey;
+            Image[] oldImage = new Image[spm.nbrfantome];
+
             for (int i = 0; i < SIZE_X; i++) { // rafraichissement graphique 
                 for (int j = 0; j < SIZE_Y; j++) {
-                    for(int k=0;k<spm.nbrfantome;k++){
-                        if(fantomex[k]==i && fantomey[k]==j){
-                            switch (k){
-                                case 0: tab[i][j].setImage(imGhost0);
-                                    break;
-                                case 1: tab[i][j].setImage(imGhost1);
-                                    break;
-                                case 2: tab[i][j].setImage(imGhost2);
-                                    break;
-                                case 3: tab[i][j].setImage(imGhost3);
-                                    break;
-                            }
-
-                        }
-                    }
                     if (spm.getX() == i && spm.getY() == j) { // spm est à la position i, j => le dessiner
                         tab[i][j].setImage(imPM);
+                    }
+                    oldfantomex = fantomex;
+                    oldfantomey = fantomey;
+                    for (int k = 0; k < spm.nbrfantome; k++) {
+                        if (fantomex[k] == i && fantomey[k] == j) {
+                            oldImage[k] = tab[i][j].getImage();
+                            switch (k) {
+                                case 0:
+                                    if (spm.estInvincible())
+                                        tab[i][j].setImage(imGhostAfraid);
+                                    else
+                                        tab[i][j].setImage(imGhost0);
+                                    break;
+                                case 1:
+                                    if (spm.estInvincible())
+                                        tab[i][j].setImage(imGhostAfraid);
+                                    else
+                                        tab[i][j].setImage(imGhost1);
+                                    break;
+                                case 2:
+                                    if (spm.estInvincible())
+                                        tab[i][j].setImage(imGhostAfraid);
+                                    else
+                                        tab[i][j].setImage(imGhost2);
+                                    break;
+                                case 3:
+                                    if (spm.estInvincible())
+                                        tab[i][j].setImage(imGhostAfraid);
+                                    else
+                                        tab[i][j].setImage(imGhost3);
+                                    break;
+                            }
+                            spm.deplacementfantome(k);
+                            if (!spm.caseAvecFantome()) {
+                                tab[oldfantomex[k]][oldfantomey[k]].setImage(oldImage[k]);
+                            }
+                        }
                     }
                 }
             }
 
-            if(oldx!=spm.getX() || oldy!=spm.getY())
+            if (oldx != spm.getX() || oldy != spm.getY())
                 tab[oldx][oldy].setImage(imSol);
-            if(spm.getTab(spm.getX(),spm.getY())==2){
+
+            if (spm.getTab(spm.getX(), spm.getY()) == 2) {
                 spm.augmenterScore(0);
-                spm.setTab(spm.getX(),spm.getY(),0);
-            } else if(spm.getTab(spm.getX(),spm.getY())==3){
+                spm.setTab(spm.getX(), spm.getY(), 0);
+            } else if (spm.getTab(spm.getX(), spm.getY()) == 3) {
                 spm.augmenterScore(1);
                 spm.setInvincible();
-                spm.setTab(spm.getX(),spm.getY(),0);
+                spm.setTab(spm.getX(), spm.getY(), 0);
             }
 
             oldx = spm.getX();
             oldy = spm.getY();
 
+            if (spm.estInvincible()) {
+                spm.decrementerTimer();
+            }
             spm.collisionFantome();
             spm.deplacement(direction);
-            spm.deplacementfantome();
-        }; 
-         
-        spm.addObserver(o); 
+        };
+
+        spm.addObserver(o);
         spm.start(); // on démarre spm 
 
         Scene scene = new Scene(root, 500, 500);
-         
-        primaryStage.setTitle("PacMan"); 
-        primaryStage.setScene(scene); 
-        primaryStage.show(); 
- 
+
+        primaryStage.setTitle("PacMan");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
         // on écoute le clavier 
         root.setOnKeyPressed(event -> {
-            direction=event.getCode();
-        }); 
-         
+            direction = event.getCode();
+        });
+
         root.requestFocus();
         primaryStage.setOnCloseRequest((WindowEvent event1) -> {
             spm.stop();
         });
-         
+
     }
 
     /**
-     * @param args the command line arguments 
-     */ 
-    public static void main(String[] args) { 
-        launch(args); 
-    } 
-     
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 } 
